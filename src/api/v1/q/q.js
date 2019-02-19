12c5843ca86869556fa3c18e567aeac00340dd4b
ThaiWu <thaiwu@clctech.co>
2019-02-19 14:51:51 +0800

    change Tommow

--- src/api/v1/q/q.js
+++ src/api/v1/q/q.js
@@ -1,37 +1,19 @@
-const _ = require('lodash');
-const moment = require('moment');
+// const _ = require('lodash');
+// const moment = require('moment');
 
-const mappingBigType = ['wood', 'stone', 'flax', 'monster', 'product', 'unusual', 'weapon', 'arms'];
 exports.test = async (request, reply) => {
-    const db = this.ArangoManager;
-    const data = request.query.d.toString();
-    const d = _.split(data, ',');
-    let BigType;
-    let data;
-
-    const rawObj = {};
-    _.forEach(d, (adata) => {
-        const rawData = _.split(adata, '@:');
-        switch (rawData[0]) {
-            case '@D':
-                data = rawData[1];
-                break;
-            case '@T':
-                rawObj.Time = rawData[1];
-                break;
-            case '@BT':
-                BigType = mappingBigType[rawData[1]];
-                rawObj.BigType = rawData[1];
-                break;
-            case '@ST':
-                rawObj.SmallType = rawData[1];
-                break;
-            case '@C':
-                rawObj.Cost = rawData[1];
-                break;
-        }
+    const data = request.body;
+    const date = data.date ? `FILTER p.Date == ${data.date}` : '';
+    const smallType = data.SmallType ? `FILTER p.SmallType == ${data.SmallType}` : '';
+    const rawData = await this.ArangoManager.query(aql`
+        FOR p IN ${data.BigType}
+        ${smallType}
+        ${date}
+        RETURN p
+    `).then((cur) => cur.all()).then((d) => {
+        return d;
+    }).catch((err) => {
+        throw new StorageError(err);
     });
-    const collection = await db.collection(BigType);
-    await collection.save(rawObj);
-    reply.send('ok');
+    reply.send(rawData);
 };
