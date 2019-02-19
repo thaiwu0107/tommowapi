fe74c0c58cb4c14b98d4d3ff11d63c398ca2eac6
ThaiWu <thaiwu@clctech.co>
2019-02-19 15:55:50 +0800

    change Tommow

--- src/api/v1/q/q.js
+++ src/api/v1/q/q.js
@@ -14,8 +14,8 @@ exports.q = async (request, reply) => {
         }).catch((err) => {
             throw err;
         });
-        reply.send(rawData);
-    } else if (_.isUndefined(request.query.Date) && !_.isUndefined(request.query.SmallType)) {
+        reply.send(_.sortBy(rawData, ['Time']));
+    } else if (!_.isUndefined(request.query.SmallType) && _.isUndefined(request.query.Date)) {
         const rawData = await this.ArangoManager.query(aql`
             FOR p IN ${collection}
             FILTER p.SmallType == ${request.query.SmallType}
@@ -25,17 +25,29 @@ exports.q = async (request, reply) => {
         }).catch((err) => {
             throw err;
         });
-        reply.send(rawData);
-    } else if (_.isUndefined(request.query.SmallType)) {
+        reply.send(_.sortBy(rawData, ['Time']));
+    } else if (!_.isUndefined(request.query.Date) && _.isUndefined(request.query.SmallType)) {
         const rawData = await this.ArangoManager.query(aql`
             FOR p IN ${collection}
-            FILTER p.Date == ${request.query.date}
+            FILTER p.Date == ${request.query.Date}
             RETURN p
         `).then((cur) => cur.all()).then((d) => {
             return d;
         }).catch((err) => {
             throw err;
         });
-        reply.send(rawData);
+        reply.send(_.sortBy(rawData, ['Time']));
+    } else if (!_.isUndefined(request.query.Date) && !_.isUndefined(request.query.SmallType)) {
+        const rawData = await this.ArangoManager.query(aql`
+            FOR p IN ${collection}
+            FILTER p.Date == ${request.query.Date}
+            FILTER p.SmallType == ${request.query.SmallType}
+            RETURN p
+        `).then((cur) => cur.all()).then((d) => {
+            return d;
+        }).catch((err) => {
+            throw err;
+        });
+        reply.send(_.sortBy(rawData, ['Time']));
     }
 };
